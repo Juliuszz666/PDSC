8139cafb33f8b4c669d9bd152122c0828ead7241
Juliuszz666 <juliuszxxbednarek@gmail.com>
2024-03-07 16:09:17 +0100

    added old sorting - some improvents (especially documentation) needed

--- Hanoi/hanoi.c
+++ Hanoi/hanoi.c
@@ -3,11 +3,12 @@
 #include "primlib.h"
 #include <stdlib.h>
 #include <unistd.h>
+#include <assert.h>
 /*INCLUDES END*/
 /*DEFINES BEGIN*/
 #define SCREEN_WIDTH gfx_screenWidth()
 #define SCREEN_HEIGTH gfx_screenHeight()
-#define DISC_NO 12
+#define DISC_NO 4
 #define DISC_HEIGHT 20
 #define PEG_NO 3
 #define DISC_WIDTH_MAX SCREEN_WIDTH/((3*PEG_NO)+1)
@@ -30,6 +31,11 @@ typedef struct
 	point rightDown;
 }rect;
 /*STRUCTS END*/
+/*GLOBAL VARIABLES BEGIN*/
+static int top[PEG_NO];
+/*Matrix used to store actucal position of discs*/
+rect stacks[PEG_NO][DISC_NO];
+/*GLOBAL VARIABLES END*/
 /*FUNCTIONS DECLARATIONS BEGIN*/
 
 /*Function computes position of pegs on screen*/
@@ -39,6 +45,9 @@ void initializePegs(rect pegs[]);
 /*@param disc structures where position of discs is stored*/
 /*@param pegs position of pegs*/
 void initializeDiscs(rect discs[], rect pegs[]);
+/*Funtion pops discs from a stack*/
+/*@param index number of peg on which we operate*/
+rect popDisc(int index);
 /*FUNCTIONS DECLARATIONS END*/
 /*MAIN FUNCTION BEGIN*/
 int main(int argc, char* argv[])
@@ -67,7 +76,47 @@ int main(int argc, char* argv[])
 		}
 		
 		gfx_updateScreen();
-		gfx_pollkey();
+		int key = gfx_getkey();
+		switch (key)
+		{
+		case SDLK_ESCAPE:
+			exit(1);
+			break;
+		case SDLK_1:
+			printf("Cos\n");
+			break;
+		case SDLK_2:
+			printf("Cos\n");
+			break;
+		case SDLK_3:
+			printf("Cos\n");
+			break;
+		case SDLK_4:
+			printf("Cos\n");
+			break;
+		case SDLK_5:
+			printf("Cos\n");
+			break;
+		case SDLK_6:
+			printf("Cos\n");
+			break;
+		case SDLK_7:
+			printf("Cos\n");
+			break;
+		case SDLK_8:
+			printf("Cos\n");
+			break;
+		case SDLK_9:
+			printf("Cos\n");
+			break;
+		case SDLK_0:
+			printf("Cos\n");
+			break;
+		default:
+			printf("Undefined behavior\n");
+			exit(1);
+			break;
+		}
 	}
 
 	return 0;
@@ -94,6 +143,10 @@ void initializeDiscs(rect discs[], rect pegs[])
 		discs[i].rightDown.y = pegs[0].rightDown.y - (DISC_HEIGHT*i);
 		discs[i].leftUpper.y = pegs[0].rightDown.y - (DISC_HEIGHT*(i+1));
 	}
-
+}
+rect popDisc(int index)
+{
+	assert(top[index]>0);
+	return stacks[index][--top[index]];
 }
 /*FUNCTION BODY END*/
---
+++ QuickSort_with_names/qs_with_names.c
@@ -0,0 +1,203 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+#define STR_SIZE 25
+#define SEC_BUFFER 128
+
+/*Struct for storing data*/
+typedef struct
+{
+    char name[STR_SIZE];
+    char surname[STR_SIZE];
+}person;
+
+/*Swapping values of struct with given indexes*/
+void swap(person* array, int index_1, int index_2);
+
+/*Component of quicksorting*/
+int dividePivot(person* array, int left, int right);
+
+/*Sorting names. Firstly it sort by surname (second string) then by name (if surnames are the same)*/
+void quickSort(person* array, int left, int right);
+
+/*
+Function responsible for getting input from a .txt file.
+Function handles:
+-too long input (it only returns lines that would not overflow person struct)
+-- in addition function prints lines where invalid input occured
+-invalid file and its extention
+*/
+person* getInput(const char *filename, int* lines);
+
+/*Saving names*/
+void saveNames(person* array, const char *filename, int lines);
+
+/*
+Function return expanded array by 1 if it is possible, otherwise it terminates program
+parameter file is to close file in case of unsuccessful reallocing
+*/
+person* expandInput(person* array, int size, FILE* file);
+
+int main(int argc, char const *argv[])
+{
+    if(argc!=3)
+    {
+        printf("Invalid number of arguments\n");
+        exit(1);
+    }
+    int lines = 0;
+    person* input = getInput(argv[1], &lines);
+    quickSort(input, 0, lines - 1);
+    saveNames(input, argv[2], lines);
+    free(input);
+
+    return 0;
+}
+
+void saveNames(person* array, const char *filename, int lines)
+{
+    if (strstr(filename, ".txt") == NULL)
+    {
+        perror("Invalid filename");
+        exit(1);
+    }
+    FILE *output_file;
+    output_file = fopen(filename, "w");
+    for (int i = 0; i < lines; i++)
+    {
+        fprintf(output_file, "%s %s\n", array[i].name, array[i].surname);
+    }
+
+    fclose(output_file);
+}
+
+person* getInput(const char *filename, int* lines)
+{
+    if (strstr(filename, ".txt") == NULL)
+    {
+        printf("Invalid filename\n");
+        exit(1);
+    }
+
+    FILE *input_file;
+    input_file = fopen(filename, "r");
+
+    if (input_file == NULL)
+    {
+        perror("Invalid file");
+        exit(1);
+    }
+
+    person* input = NULL;
+
+    int line = 0;
+    int file_line = 0;
+    char first_buffer[SEC_BUFFER];
+
+    while (fgets(first_buffer, SEC_BUFFER, input_file))
+    {
+        /*Initial verification of input*/
+        file_line++;
+        if(strlen(first_buffer)==1)
+        {
+            printf("Invalid line no: %d\t - empty line\n", file_line);
+            continue;
+        }
+        else if(strstr(first_buffer," ")==NULL)
+        {
+            printf("Invalid line no: %d\t - no space\n", file_line);
+            continue;
+        }
+
+        char buffer[2][STR_SIZE];
+        /*No defense against line with wrongly placed space*/
+        sscanf(first_buffer, "%s %s", buffer[0], buffer[1]);
+        bool noOverflow = ((strlen(buffer[0]) < STR_SIZE - 1) && (strlen(buffer[1]) < STR_SIZE - 1));
+        if(!noOverflow)
+        {
+            printf("Invalid line no: %d\t - too long name of surname\n", file_line);
+            continue;
+        }
+        else
+        {
+            input = expandInput(input, line, input_file);
+            strcpy(input[line].name, buffer[0]);
+            strcpy(input[line].surname, buffer[1]);
+            line++;
+        }
+    }
+    fclose(input_file);
+
+    *lines = line;
+
+    return input;
+}
+
+void quickSort(person* array, int left, int right)
+{
+
+    if (left < right)
+    {
+        int pivot = dividePivot(array, left, right);
+        quickSort(array, left, pivot - 1);
+        quickSort(array, pivot + 1, right);
+    }
+}
+
+int dividePivot(person* array, int left, int right)
+{
+    int diveder_index = left + ((right - left) / 2);
+    person diveder = array[diveder_index];
+
+
+    /*Moving diveder index to the right*/
+    swap(array, diveder_index, right);
+
+    int current_pos = left;
+
+    for (int i = left; i < right; i++)
+    {
+        bool surnameToSwap = strcmp(array[i].surname, diveder.surname) < 0;
+        bool nameToSawp = strcmp(array[i].surname, diveder.surname) == 0 && strcmp(array[i].name, diveder.name) < 0;
+        if (surnameToSwap||nameToSawp)
+        {
+            swap(array, current_pos, i);
+            current_pos++;
+        }
+    }
+
+    /*Returning diveder index to its destined posistion*/
+    swap(array, current_pos, right);
+
+    return current_pos;
+}
+
+void swap(person* array, int index_1, int index_2)
+{
+    char buffer[STR_SIZE];
+    memmove(buffer, array[index_1].name, STR_SIZE);
+    memmove(array[index_1].name, array[index_2].name, STR_SIZE);
+    memmove(array[index_2].name, buffer, STR_SIZE);
+
+    memmove(buffer, array[index_1].surname, STR_SIZE);
+    memmove(array[index_1].surname, array[index_2].surname, STR_SIZE);
+    memmove(array[index_2].surname, buffer, STR_SIZE);
+}
+
+person* expandInput(person* array, int size, FILE* file)
+{
+    person* temp_val = (person*) realloc(array, sizeof(person)*(size+1));
+    if(temp_val==NULL)
+    {
+        printf("No memory available, possibility of memory leak\n");
+        free(array);
+        fclose(file);
+        exit(1);
+    }
+    else
+    {
+        return temp_val;
+    }
+}
